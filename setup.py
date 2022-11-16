6eb91e04519b96bf8a6358c93b91439652520560
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-11-16 13:58:21 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md","r",encoding = "utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Credit_Card_Defaulters_Prediction"
+AUTHOR_NAME = "Shivansh Kaushal"
+AUTHOR_USER_NAME = "shivanshka"
+SRC_REPO = "Application"
+AUTHOR_EMAIL = "kaushal.shivansh630@gmail.com"
+
+setuptools.setup(
+    name = SRC_REPO,
+    version= __version__,
+    author = AUTHOR_NAME,
+    author_email= AUTHOR_EMAIL,
+    description= "This application predicts whether a individual defaults from paying credit card installments",
+    long_description=long_description,
+    long_description_content = "text/markdown",
+    url= f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "Bug Tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir= {"":"src"},
+    packages=setuptools.find_packages(where='src') 
+)
